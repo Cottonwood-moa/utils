1c219dc3b8e3a44dc480d80ff6b74d018b893eeb
Cottonwood <blog_geon@naver.com>
2023-01-15 22:40:51 +0900

    Revert "revert"
    This reverts commit 385e666193ae6f6d6cb7d53e693513f3e6ae45fc.

--- randomString.ts
+++ randomString.ts
@@ -1,14 +1,16 @@
+
 /**
- * @param len (number) 만들려는 문자열 길이
- * @return (string) 랜덤 문자열
+ * @param len 만들려는 문자열 길이
+ * @return r
  */
-function randomString(len: number): string {
-  let result = "";
-  const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  const charactersLength = characters.length;
-  for (let i = 0; i < len; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+function randomString(len: number) {
+    let result = '';
+    const characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < len; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
   }
-  return result;
-}
+
