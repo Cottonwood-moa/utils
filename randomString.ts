43a53cd447dc95edcf2e041691538164e16e4d5f
Cottonwood <blog_geon@naver.com>
2023-01-15 22:40:36 +0900

    revert

--- randomString.ts
+++ randomString.ts
@@ -1,16 +1,14 @@
-
 /**
- * @param len 만들려는 문자열 길이
- * @return r
+ * @param len (number) 만들려는 문자열 길이
+ * @return (string) 랜덤 문자열
  */
-function randomString(len: number) {
-    let result = '';
-    const characters =
-      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
-    for (let i = 0; i < len; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
+function randomString(len: number): string {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  for (let i = 0; i < len; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
-
+  return result;
+}
