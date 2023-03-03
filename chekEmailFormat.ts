2b0db68bd7eaa13b62d20906eb1b982cd9ff7776
gw_park <gw.park@twolinecode.com>
2023-03-03 15:18:04 +0900

    test

--- chekEmailFormat.ts
+++ chekEmailFormat.ts
@@ -25,3 +25,6 @@ function checkEmailFormat(email) {
     const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return emailRegex.test(email);
   } */
+/* 
+test
+*/
